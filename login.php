9b550563d1391cf272d78c6281bacd7c69552a76
Muhammad Hasbiallah <129852622+Mhasbi27@users.noreply.github.com>
2024-05-06 16:54:12 +0700

    login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
+include 'db.php';
 session_start();
-
 
 if (isset($_SESSION['user_id'])) {
     header('Location: home.php');
@@ -13,20 +13,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Koneksi database
     $conn = new PDO("mysql:host=localhost;dbname=event", 'root', '');
 
-    $stmt = $conn->prepare("SELECT user_id, password FROM users WHERE email = ?");
+    $stmt = $conn->prepare("SELECT user_id, password, role FROM users WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch();
 
     if ($user && password_verify($password, $user['password'])) {
-        // Login sukses
+        
         $_SESSION['user_id'] = $user['user_id'];
-        header('Location: home.php');
+        $_SESSION['role'] = $user['role']; 
+
+
+        if ($_SESSION['role'] === 'admin') {
+            header('Location: admin_dashboard.php');
+        } else {
+            header('Location: home.php');
+        }
         exit();
     } else {
-        // Login gagal
+        
         $errorMessage = 'Invalid email or password.';
     }
 }
@@ -49,15 +55,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <p style="color: red;"><?= $errorMessage ?></p>
         <?php endif; ?>
         <form method="post" action="login.php">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-
-    <input type="submit" value="Login">
-</form>
-
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <input type="submit" value="Login">
+        </form>
     </div>
 </body>
 </html>
